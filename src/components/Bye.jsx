521aa3dc70077e4c7ab5d59fc895bda8e90890bf
Jongkyun <hjk326333@gmail.com>
2025-04-08 12:15:04 +0900

    컴포넌트 기초, 지출항목 컴포넌트 생성

---
+++ src/components/Bye.jsx
@@ -0,0 +1,14 @@
+// rafce
+import React from 'react';
+
+const Bye = () => {
+  return (
+    <ul>
+      <li>사과</li>
+      <li>딸기</li>
+      <li>오렌지</li>
+    </ul>
+  );
+};
+
+export default Bye;
